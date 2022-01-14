9b24362edef55b53a7a5be584ee5bd40d9a721e4
Sebastian <sebastianfox81@gmail.com>
2022-01-14 16:33:13 -0500

    Implemented useStylin for all  components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from './images/img1.png'
+import memories from './images/img1.png';
+import useStyles from './styles'
 
 // Components
 import Form from './components/Form/Form.js'
 import Posts from './components/Posts/Posts.js'
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
