537a31d839c810afee91c8675d06f615580a052e
Sebastian <sebastianfox81@gmail.com>
2022-01-15 15:01:39 -0500

    Completed form layout/styling

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,11 +1,11 @@
 import * as api from '../api';
 
 // Action Creators
-  const getPosts = () => async (dispatch) => {
+  export const getPosts = () => async (dispatch) => {
     try {
       const { data } = await api.fetchPosts();
       dispatch({ type: 'FETCH_ALL', payload: data});
     } catch (err) {
-      
+      console.log(err.message)
     }
   }
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,68 @@
-import React from 'react'
-import useStyles from './styles';
+import React, { useState } from "react";
+import useStyles from "./styles";
+import FileBase from 'react-file-base64';
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+
 const Form = () => {
+  const [ postData, setPostData ] = useState({
+    creator: '',
+    title: '',
+    msg: '',
+    tags: '',
+    selectedFile: ''
+  })
+
   const classes = useStyles();
+
+  const handleChange = (e) => {
+    setPostData({
+      ...postData, [e.target.name]: e.target.value
+    })
+  }
+
+  const clear = () => {
+
+  }
+
   return (
-    <div>
-      <h1>Form</h1>
-    </div>
-  )
-}
+    <Paper className={classes.paper}>
+      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`}>
+        <Typography variant="h6">Creating Memory</Typography>
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={handleChange} />
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={handleChange} />
+        <TextField name="msg" variant="outlined" label="Message" fullWidth value={postData.msg} onChange={handleChange} />
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={handleChange} />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64})}
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          size="large"
+          color="primary"
+          type="submit"
+          fullWidth>
+            Submit
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          color="secondary"
+          onClick={clear}
+          fullWidth>
+            Clear
+        </Button>
+      </form>
+    </Paper>
+  );
+};
 
 export default Form;
+
+
+
+
